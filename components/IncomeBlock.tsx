1db025f20fbd2a7ec1066041a495a91fb90da0be
mubashir malik <malikmubashir601@gmail.com>
2024-10-05 14:11:10 +0500

    expense block created

---
+++ components/IncomeBlock.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function IncomeBlock() {
+  return (
+    <View>
+      <Text>IncomeBlock</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
